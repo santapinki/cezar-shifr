c9a4d58942327150bbfad29c1478b7c46f73bc1e
santapinki <5345119@mail.ru>
2022-01-18 22:09:15 +0300

    initial commit

--- src/aga.java
+++ src/aga.java
@@ -11,13 +11,18 @@ public class aga {
     static Map<Integer, Character> hashMapOver = new HashMap<>();// Упордечная мапа номер и его символов 1 а    2 б
     static Map<Character,Integer> hashMapNotOver = new HashMap<>(); // Упорядочная мапа символ и его номер а 1 , б 2
     static Map<Character,Integer> hashMapProcent = new HashMap<>(); // Мапа в проентах ищем частвый символ это пробел
+    static String nameFile = "test.txt";   // Имя Файла оригинал не зашифрованый
+    static String nameFileNew = "notes4.txt"; // ИмяФайла который будет зашифрованый уже
+    static String nameFileForDeshefrovka = "notes6.txt"; // ИмяФайла который будет расшифрован
+    static int smeshenie=1;
 
 
     public static void main(String[] args) throws IOException {
         napolnenieMap()    ;
-        //  podchetSimvolov(readFile());
-        //   shifr(readFile());
-        // reshifrofka(readFile());
+         // podchetSimvolov(readFile());     // Нужно внутри методов
+      //    shifr(readFile());              // Шифруем файл
+         reshifrofka(readFile());              // Расшифровываем файл не зная смещение
+     //   rashifrovka(readFileForDeshefrofka(),smeshenie);// расшифровка зная смешение
     }
 
     public static void shifr(List<String> spisok) throws IOException {
@@ -32,7 +37,8 @@ public class aga {
 
         ArrayList<String> outputSpisok=new ArrayList<>() ;
         String overwrite="";
-        FileWriter writer = new FileWriter("Q:\\filetest\\lvl31\\notes3.txt", true);
+        //FileWriter writer = new FileWriter("Q:\\filetest\\lvl31\\notes3.txt", true);
+        FileWriter writer = new FileWriter(nameFileNew);
         for (String stroka : spisok) {
             char[] strokaChar = stroka.toLowerCase().toCharArray();
             for (int i = 0; i <strokaChar.length ; i++) {
@@ -58,7 +64,7 @@ public class aga {
         }
         writer.flush();
         writer.close();
-    }
+    }         // Метод защифровывает
     public static void podchetSimvolov(List<String> strings){
         for (String stroka : strings) {
             char[] strokaChar = stroka.toCharArray();
@@ -92,7 +98,7 @@ public class aga {
 
         }
 
-    }
+    }         // Считаем символы что бы расшифровать методом подбора
     public static void napolnenieMap(){
         char[] bazaSim = {'а','б','в','г','д','е','ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '”', ':', '-', '!', '?', ' '};
 
@@ -105,18 +111,20 @@ public class aga {
         for (int i = 0; i < bazaSim.length; i++) {
             hashMapNotOver.put(bazaSim[i], i);
         }
-    }
+    }          // Всегда надо запускать
     public static List<String> readFile() throws IOException {
-        String nameFile = "test.txt";
+        // String nameFile = "test.txt";
         //   String nameFile = "notes4.txt";
         String nameDirectory = "Q:\\filetest\\lvl31";
         Path pathDirectory = Path.of(nameDirectory);
         Path filePath = Path.of(nameFile);
-        List<String> strings = Files.readAllLines(pathDirectory.resolve(filePath), Charset.forName("windows-1251"));
+        // List<String> strings = Files.readAllLines(pathDirectory.resolve(filePath), Charset.forName("windows-1251"));
+        List<String> strings = Files.readAllLines(filePath, Charset.forName("windows-1251"));
         return strings;
-    }
-    public static void reshifrofka(List<String> spisok){
+    }  // Читаем файл для шифровки
+    public static void reshifrofka(List<String> spisok) throws IOException {
         podchetSimvolov(spisok);
+        FileWriter writer = new FileWriter(nameFileForDeshefrovka);
         int probel=0;
         char simvol=' ';
         int smechenie=0;
@@ -155,5 +163,56 @@ public class aga {
             ss="";
 
         }
+        for (String s:pavilno) {
+            writer.write(s);
+            writer.write('\n');
+        }
+        writer.flush();
+        writer.close();
+    } // Расшифровка не знаем смещенеие
+    public static void rashifrovka(List<String>spisok,int smechenie) throws IOException {
+        System.out.println(smechenie);
+        String ss="";
+        FileWriter writer = new FileWriter(nameFileForDeshefrovka);
+        ArrayList<String> pavilno = new ArrayList();
+        for (String s :spisok ) {
+            char[] mass= s.toCharArray();
+            for (int i = 0; i <mass.length ; i++) {
+
+                if (hashMap.get(mass[i])!=null) {
+                    if (hashMapNotOver.get(mass[i])-smechenie<0) {
+                        ss = ss + hashMapOver.get(41 - smechenie);
+                    }else{
+
+                        ss = ss + hashMapOver.get(hashMapNotOver.get(mass[i])-smechenie);
+                    }
+                }else{
+                    ss =ss+mass[i];
+                }
+
+            }
+            System.out.println(ss);
+            pavilno.add(ss);
+            ss="";
+
+        }
+        for (String s:pavilno) {
+            writer.write(s);
+            writer.write('\n');
+        }
+        writer.flush();
+        writer.close();
+    } // Расшифровка знаем смещение
+    public static List<String> readFileForDeshefrofka() throws IOException {
+      // String nameFile = "test.txt";
+        //   String nameFile = "notes4.txt";
+//        String nameDirectory = "Q:\\filetest\\lvl31";
+//        Path pathDirectory = Path.of(nameDirectory);
+
+        // List<String> strings = Files.readAllLines(pathDirectory.resolve(filePath), Charset.forName("windows-1251"));
+        Path filePath = Path.of(nameFileNew);
+      //  List<String> strings = Files.readAllLines(filePath, Charset.forName("windows-1251"));
+        List<String> strings = Files.readAllLines(filePath);
+        return strings;
     }
 }
